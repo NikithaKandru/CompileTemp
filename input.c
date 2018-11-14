ca4c622b708643a97d5eaa2e4189af0718cae954
nkandru <kandru.nikitha@gmail.com>
2018-11-14 12:32:38 -0500

    f

---
+++ input.c
@@ -0,0 +1,19 @@
+
+
+
+int main(){
+	int i =0;
+	int a[10];
+	for(i =0; i<10; i++){
+	    a[i] = i;
+	    for(int j=0; j<i;j++){    
+		a[j] = j*2;
+	    }
+        cout<<"taske one done"<<"\n";
+	}	
+
+	for(int k=0; k<10; k++){
+	   a[i] = a[i]-1;
+	}
+	return 0;
+}
